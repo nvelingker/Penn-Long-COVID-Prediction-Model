25d32a8eb8c2235883349c9ae00853c92cca1f99
Foundry (vector-client-token) <>
2022-10-18 19:27:59 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -46,3 +46,293 @@ def customized_concept_set_input_testing(LL_concept_sets_fusion_everyone, LL_DO_
 ## Global imports and functions included below ##
 #################################################
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.8dc65c1f-39e5-4bb7-b5c0-161a2f87aa0e"),
+    concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
+    location=Input(rid="ri.foundry.main.dataset.4805affe-3a77-4260-8da5-4f9ff77f51ab"),
+    manifest_safe_harbor=Input(rid="ri.foundry.main.dataset.b4407989-1851-4e07-a13f-0539fae10f26"),
+    microvisits_to_macrovisits=Input(rid="ri.foundry.main.dataset.d77a701f-34df-48a1-a71c-b28112a07ffa"),
+    person=Input(rid="ri.foundry.main.dataset.f71ffe18-6969-4a24-b81c-0e06a1ae9316")
+)
+#Purpose - The purpose of this pipeline is to produce a visit day level and a persons level fact table for all patients in the N3C enclave. More information can be found in the README linked here (https://unite.nih.gov/workspace/report/ri.report.main.report.855e1f58-bf44-4343-9721-8b4c878154fe).
+#Creator/Owner/contact - Andrea Zhou
+#Last Update - 5/6/22
+#Description - This node gathers some commonly used facts about these patients from the "person" and "location" tables, as well as some facts about the patient's institution (from the "manifest" table).  Available age, race, and locations data (including SDOH variables for L3 only) is gathered at this node.  The patient’s total number of visits as well as the number of days in their observation period is calculated from the “microvisits_to_macrovisits” table in this node.  These facts will eventually be joined with the final patient-level table in the final node.
+
+def everyone_cohort_de_id(concept_set_members, person, location, manifest_safe_harbor, microvisits_to_macrovisits):
+        
+    """
+    Select proportion of enclave patients to use: A value of 1.0 indicates the pipeline will use all patients in the persons table.  
+    A value less than 1.0 takes a random sample of the patients with a value of 0.001 (for example) representing a 0.1% sample of the persons table will be used.
+    """
+    proportion_of_patients_to_use = 1.0
+
+    concepts_df = concept_set_members
+    
+    person_sample = person \
+        .select('person_id','year_of_birth','month_of_birth','day_of_birth','ethnicity_concept_name','race_concept_name','gender_concept_name','location_id','data_partner_id') \
+        .distinct() \
+        .sample(False, proportion_of_patients_to_use, 111)
+
+    visits_df = microvisits_to_macrovisits.select("person_id", "macrovisit_start_date", "visit_start_date")
+
+    manifest_df = manifest_safe_harbor \
+        .select('data_partner_id','run_date','cdm_name','cdm_version','shift_date_yn','max_num_shift_days') \
+        .withColumnRenamed("run_date", "data_extraction_date")
+
+    location_df = location \
+        .dropDuplicates(subset=['location_id']) \
+        .select('location_id','city','state','zip','county') \
+        .withColumnRenamed('zip','postal_code')   
+    
+    #join in location_df data to person_sample dataframe 
+    df = person_sample.join(location_df, 'location_id', 'left')
+
+    #join in manifest_df information
+    df = df.join(manifest_df, 'data_partner_id','inner')
+    df = df.withColumn('max_num_shift_days', F.when(F.col('max_num_shift_days')=="", F.lit('0')).otherwise(F.regexp_replace(F.lower('max_num_shift_days'), 'na', '0')))
+    
+    #calculate date of birth for all patients
+    df = df.withColumn("new_year_of_birth", F.when(F.col('year_of_birth').isNull(),1)
+                                                .otherwise(F.col('year_of_birth')))
+    df = df.withColumn("new_month_of_birth", F.when(F.col('month_of_birth').isNull(), 7)
+                                                .when(F.col('month_of_birth')==0, 7)
+                                                .otherwise(F.col('month_of_birth')))
+    df = df.withColumn("new_day_of_birth", F.when(F.col('day_of_birth').isNull(), 1)
+                                                .when(F.col('day_of_birth')==0, 1)
+                                                .otherwise(F.col('day_of_birth')))
+
+    df = df.withColumn("date_of_birth", F.concat_ws("-", F.col("new_year_of_birth"), F.col("new_month_of_birth"), F.col("new_day_of_birth")))
+    df = df.withColumn("date_of_birth", F.to_date("date_of_birth", format=None)) 
+
+    #convert date of birth string to date and apply min and max reasonable birthdate filter parameters, inclusive
+    max_shift_as_int = df.withColumn("shift_days_as_int", F.col('max_num_shift_days').cast(IntegerType())) \
+        .select(F.max('shift_days_as_int')) \
+        .head()[0]
+
+    min_reasonable_dob = "1902-01-01"
+    max_reasonable_dob = F.date_add(F.current_date(), max_shift_as_int)
+
+    df = df.withColumn("date_of_birth", F.when(F.col('date_of_birth').between(min_reasonable_dob, max_reasonable_dob), F.col('date_of_birth')).otherwise(None))
+
+    df = df.withColumn("age", F.floor(F.months_between(max_reasonable_dob, "date_of_birth", roundOff=False)/12))
+
+    H = ['Hispanic']
+    A = ['Asian', 'Asian Indian', 'Bangladeshi', 'Bhutanese', 'Burmese', 'Cambodian', 'Chinese', 'Filipino', 'Hmong', 'Indonesian', 'Japanese', 'Korean', 'Laotian', 'Malaysian', 'Maldivian', 'Nepalese', 'Okinawan', 'Pakistani', 'Singaporean', 'Sri Lankan', 'Taiwanese', 'Thai', 'Vietnamese']
+    B_AA = ['African', 'African American', 'Barbadian', 'Black', 'Black or African American', 'Dominica Islander', 'Haitian', 'Jamaican', 'Madagascar', 'Trinidadian', 'West Indian']
+    W = ['White']
+    NH_PI = ['Melanesian', 'Micronesian', 'Native Hawaiian or Other Pacific Islander', 'Other Pacific Islander', 'Polynesian']
+    AI_AN = ['American Indian or Alaska Native']
+    O = ['More than one race', 'Multiple race', 'Multiple races', 'Other', 'Other Race']
+    U = ['Asian or Pacific Islander', 'No Information', 'No matching concept', 'Refuse to Answer', 'Unknown', 'Unknown racial group']
+
+    df = df.withColumn("race_ethnicity", F.when(F.col("ethnicity_concept_name") == 'Hispanic or Latino', "Hispanic or Latino Any Race")
+                        .when(F.col("race_concept_name").isin(H), "Hispanic or Latino Any Race")
+                        .when(F.col("race_concept_name").isin(A), "Asian Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(B_AA), "Black or African American Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(W), "White Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(NH_PI), "Native Hawaiian or Other Pacific Islander Non-Hispanic") 
+                        .when(F.col("race_concept_name").isin(AI_AN), "American Indian or Alaska Native Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(O), "Other Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(U), "Unknown")
+                        .otherwise("Unknown"))
+
+    #create visit counts/obs period dataframes
+    hosp_visits = visits_df.where(F.col("macrovisit_start_date").isNotNull()) \
+        .orderBy("visit_start_date") \
+        .coalesce(1) \
+        .dropDuplicates(["person_id", "macrovisit_start_date"]) #hospital
+
+    non_hosp_visits = visits_df.where(F.col("macrovisit_start_date").isNull()) \
+        .dropDuplicates(["person_id", "visit_start_date"]) #non-hospital
+        
+    visits_df = hosp_visits.union(non_hosp_visits) #join the two
+
+    #total number of visits
+    visits_count = visits_df.groupBy("person_id") \
+        .count() \
+        .select("person_id", F.col('count').alias('total_visits'))
+
+    #obs period in days 
+    observation_period = visits_df.groupby('person_id').agg(
+        F.max('visit_start_date').alias('pt_max_visit_date'),
+        F.min('visit_start_date').alias('pt_min_visit_date')) \
+        .withColumn('observation_period', F.datediff('pt_max_visit_date', 'pt_min_visit_date')) \
+        .select('person_id', 'observation_period')
+    
+    #join visit counts/obs periods dataframes with main dataframe
+    df = df.join(visits_count, "person_id", "left")
+    df = df.join(observation_period, "person_id", "left")
+
+    #LEVEL 2 ONLY
+    df = df.withColumn('max_num_shift_days', F.concat(F.col('max_num_shift_days'), F.lit(" + 180"))).withColumn('shift_date_yn', F.lit('Y'))
+
+    df = df.select('person_id',
+        'total_visits',
+        'observation_period',
+        'gender_concept_name',
+        'city',
+        'state',
+        'postal_code',
+        'county',
+        'age',
+        'race_ethnicity',
+        'data_partner_id',
+        'data_extraction_date',
+        'cdm_name',
+        'cdm_version',
+        'shift_date_yn',
+        'max_num_shift_days')
+
+    return df
+    
+
+#################################################
+## Global imports and functions included below ##
+#################################################
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.0ca5c190-d2ae-492a-b291-66fd8092b269"),
+    concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
+    location_testing=Input(rid="ri.foundry.main.dataset.06b728e0-0262-4a7a-b9b7-fe91c3f7da34"),
+    manifest_safe_harbor_testing=Input(rid="ri.foundry.main.dataset.7a5c5585-1c69-4bf5-9757-3fd0d0a209a2"),
+    microvisits_to_macrovisits_testing=Input(rid="ri.foundry.main.dataset.f5008fa4-e736-4244-88e1-1da7a68efcdb"),
+    person_testing=Input(rid="ri.foundry.main.dataset.06629068-25fc-4802-9b31-ead4ed515da4")
+)
+#Purpose - The purpose of this pipeline is to produce a visit day level and a persons level fact table for all patients in the N3C enclave. More information can be found in the README linked here (https://unite.nih.gov/workspace/report/ri.report.main.report.855e1f58-bf44-4343-9721-8b4c878154fe).
+#Creator/Owner/contact - Andrea Zhou
+#Last Update - 5/6/22
+#Description - This node gathers some commonly used facts about these patients from the "person" and "location" tables, as well as some facts about the patient's institution (from the "manifest" table).  Available age, race, and locations data (including SDOH variables for L3 only) is gathered at this node.  The patient’s total number of visits as well as the number of days in their observation period is calculated from the “microvisits_to_macrovisits” table in this node.  These facts will eventually be joined with the final patient-level table in the final node.
+
+def everyone_cohort_de_id_testing(concept_set_members, person_testing, location_testing, manifest_safe_harbor_testing, microvisits_to_macrovisits_testing):
+        
+    """
+    Select proportion of enclave patients to use: A value of 1.0 indicates the pipeline will use all patients in the persons table.  
+    A value less than 1.0 takes a random sample of the patients with a value of 0.001 (for example) representing a 0.1% sample of the persons table will be used.
+    """
+    proportion_of_patients_to_use = 1.0
+
+    concepts_df = concept_set_members
+    
+    person_sample = person_testing \
+        .select('person_id','year_of_birth','month_of_birth','day_of_birth','ethnicity_concept_name','race_concept_name','gender_concept_name','location_id','data_partner_id') \
+        .distinct() \
+        .sample(False, proportion_of_patients_to_use, 111)
+
+    visits_df = microvisits_to_macrovisits_testing.select("person_id", "macrovisit_start_date", "visit_start_date")
+
+    manifest_df = manifest_safe_harbor_testing \
+        .select('data_partner_id','run_date','cdm_name','cdm_version','shift_date_yn','max_num_shift_days') \
+        .withColumnRenamed("run_date", "data_extraction_date")
+
+    location_df = location_testing \
+        .dropDuplicates(subset=['location_id']) \
+        .select('location_id','city','state','zip','county') \
+        .withColumnRenamed('zip','postal_code')   
+    
+    #join in location_df data to person_sample dataframe 
+    df = person_sample.join(location_df, 'location_id', 'left')
+
+    #join in manifest_df information
+    df = df.join(manifest_df, 'data_partner_id','inner')
+    df = df.withColumn('max_num_shift_days', F.when(F.col('max_num_shift_days')=="", F.lit('0')).otherwise(F.regexp_replace(F.lower('max_num_shift_days'), 'na', '0')))
+    
+    #calculate date of birth for all patients
+    df = df.withColumn("new_year_of_birth", F.when(F.col('year_of_birth').isNull(),1)
+                                                .otherwise(F.col('year_of_birth')))
+    df = df.withColumn("new_month_of_birth", F.when(F.col('month_of_birth').isNull(), 7)
+                                                .when(F.col('month_of_birth')==0, 7)
+                                                .otherwise(F.col('month_of_birth')))
+    df = df.withColumn("new_day_of_birth", F.when(F.col('day_of_birth').isNull(), 1)
+                                                .when(F.col('day_of_birth')==0, 1)
+                                                .otherwise(F.col('day_of_birth')))
+
+    df = df.withColumn("date_of_birth", F.concat_ws("-", F.col("new_year_of_birth"), F.col("new_month_of_birth"), F.col("new_day_of_birth")))
+    df = df.withColumn("date_of_birth", F.to_date("date_of_birth", format=None)) 
+
+    #convert date of birth string to date and apply min and max reasonable birthdate filter parameters, inclusive
+    max_shift_as_int = df.withColumn("shift_days_as_int", F.col('max_num_shift_days').cast(IntegerType())) \
+        .select(F.max('shift_days_as_int')) \
+        .head()[0]
+
+    min_reasonable_dob = "1902-01-01"
+    max_reasonable_dob = F.date_add(F.current_date(), max_shift_as_int)
+
+    df = df.withColumn("date_of_birth", F.when(F.col('date_of_birth').between(min_reasonable_dob, max_reasonable_dob), F.col('date_of_birth')).otherwise(None))
+
+    df = df.withColumn("age", F.floor(F.months_between(max_reasonable_dob, "date_of_birth", roundOff=False)/12))
+
+    H = ['Hispanic']
+    A = ['Asian', 'Asian Indian', 'Bangladeshi', 'Bhutanese', 'Burmese', 'Cambodian', 'Chinese', 'Filipino', 'Hmong', 'Indonesian', 'Japanese', 'Korean', 'Laotian', 'Malaysian', 'Maldivian', 'Nepalese', 'Okinawan', 'Pakistani', 'Singaporean', 'Sri Lankan', 'Taiwanese', 'Thai', 'Vietnamese']
+    B_AA = ['African', 'African American', 'Barbadian', 'Black', 'Black or African American', 'Dominica Islander', 'Haitian', 'Jamaican', 'Madagascar', 'Trinidadian', 'West Indian']
+    W = ['White']
+    NH_PI = ['Melanesian', 'Micronesian', 'Native Hawaiian or Other Pacific Islander', 'Other Pacific Islander', 'Polynesian']
+    AI_AN = ['American Indian or Alaska Native']
+    O = ['More than one race', 'Multiple race', 'Multiple races', 'Other', 'Other Race']
+    U = ['Asian or Pacific Islander', 'No Information', 'No matching concept', 'Refuse to Answer', 'Unknown', 'Unknown racial group']
+
+    df = df.withColumn("race_ethnicity", F.when(F.col("ethnicity_concept_name") == 'Hispanic or Latino', "Hispanic or Latino Any Race")
+                        .when(F.col("race_concept_name").isin(H), "Hispanic or Latino Any Race")
+                        .when(F.col("race_concept_name").isin(A), "Asian Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(B_AA), "Black or African American Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(W), "White Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(NH_PI), "Native Hawaiian or Other Pacific Islander Non-Hispanic") 
+                        .when(F.col("race_concept_name").isin(AI_AN), "American Indian or Alaska Native Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(O), "Other Non-Hispanic")
+                        .when(F.col("race_concept_name").isin(U), "Unknown")
+                        .otherwise("Unknown"))
+
+    #create visit counts/obs period dataframes
+    hosp_visits = visits_df.where(F.col("macrovisit_start_date").isNotNull()) \
+        .orderBy("visit_start_date") \
+        .coalesce(1) \
+        .dropDuplicates(["person_id", "macrovisit_start_date"]) #hospital
+
+    non_hosp_visits = visits_df.where(F.col("macrovisit_start_date").isNull()) \
+        .dropDuplicates(["person_id", "visit_start_date"]) #non-hospital
+        
+    visits_df = hosp_visits.union(non_hosp_visits) #join the two
+
+    #total number of visits
+    visits_count = visits_df.groupBy("person_id") \
+        .count() \
+        .select("person_id", F.col('count').alias('total_visits'))
+
+    #obs period in days 
+    observation_period = visits_df.groupby('person_id').agg(
+        F.max('visit_start_date').alias('pt_max_visit_date'),
+        F.min('visit_start_date').alias('pt_min_visit_date')) \
+        .withColumn('observation_period', F.datediff('pt_max_visit_date', 'pt_min_visit_date')) \
+        .select('person_id', 'observation_period')
+    
+    #join visit counts/obs periods dataframes with main dataframe
+    df = df.join(visits_count, "person_id", "left")
+    df = df.join(observation_period, "person_id", "left")
+
+    #LEVEL 2 ONLY
+    df = df.withColumn('max_num_shift_days', F.concat(F.col('max_num_shift_days'), F.lit(" + 180"))).withColumn('shift_date_yn', F.lit('Y'))
+
+    df = df.select('person_id',
+        'total_visits',
+        'observation_period',
+        'gender_concept_name',
+        'city',
+        'state',
+        'postal_code',
+        'county',
+        'age',
+        'race_ethnicity',
+        'data_partner_id',
+        'data_extraction_date',
+        'cdm_name',
+        'cdm_version',
+        'shift_date_yn',
+        'max_num_shift_days')
+
+    return df
+    
+
+#################################################
+## Global imports and functions included below ##
+#################################################
+
--- pipeline.sql
+++ pipeline.sql
@@ -81,3 +81,80 @@ where procedure_concept_id IN (766238, 766239, 766241) and po.data_partner_id = 
     and procedure_date < (m.run_date + 356*2)
     and procedure_date is not null
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.73881ed9-9110-4370-85ab-4c40e879b3ba"),
+    concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
+    drug_exposure_testing=Input(rid="ri.foundry.main.dataset.26a51cab-0279-45a6-bbc0-f44a12b52f9c"),
+    manifest_safe_harbor_testing=Input(rid="ri.foundry.main.dataset.7a5c5585-1c69-4bf5-9757-3fd0d0a209a2")
+)
+/* Import the drug_exposure_testing, concept_set_members, and manifest_safe_harbor_testing tables */
+
+SELECT distinct de.person_id,
+    de.data_partner_id,
+    de.drug_exposure_start_date,
+    de.drug_source_concept_name,
+    de.drug_concept_id,
+
+    case when concept_id in (702677, 702678, 724907, 37003432, 37003435, 37003436) then 'pfizer'
+        when concept_id in (724906, 37003518) then 'moderna'
+        when concept_id in (702866, 739906) then 'janssen'
+        when concept_id in (724905) then 'astrazeneca'
+        else null
+        end as vax_type
+    
+FROM concept_set_members cs
+INNER JOIN drug_exposure_testing de on cs.concept_id = de.drug_concept_id
+INNER JOIN manifest_safe_harbor_testing m on de.data_partner_id = m.data_partner_id
+
+where codeset_id = 600531961
+    and drug_exposure_start_date is not null
+    -- Very conservative filters to allow for date shifting
+    and drug_exposure_start_date > '2018-12-20'
+    and drug_exposure_start_date < (m.run_date + 356*2)
+    and (
+        (drug_type_concept_id not in (38000177,32833,38000175,32838,32839)) 
+        or (drug_type_concept_id = 38000177 AND m.cdm_name = 'ACT')
+    )
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.8a3be0e3-a478-40ab-83b1-7289e3fc5136"),
+    baseline_vaccines=Input(rid="ri.vector.main.execute.20bfed79-2fea-446f-a920-88f0dbc22bc2"),
+    baseline_vaccines_from_proc=Input(rid="ri.vector.main.execute.41c51a4c-2331-41c3-acad-6b3c1be58064")
+)
+--create minimal long dataset of vaccine transactions to transpose 
+
+SELECT DISTINCT person_id, 
+    data_partner_id,
+    drug_exposure_start_date as vax_date,
+    vax_type
+FROM baseline_vaccines
+
+UNION 
+
+SELECT DISTINCT person_id,
+    data_partner_id,
+    procedure_date as vax_date,
+    vax_type
+FROM baseline_vaccines_from_proc
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.de1a2a39-7020-47f6-bb12-0a2e2ccec6a1"),
+    baseline_vaccines_from_proc_testing=Input(rid="ri.vector.main.execute.9367a283-bd9d-45c9-9415-67c37443fbcc"),
+    baseline_vaccines_testing=Input(rid="ri.vector.main.execute.73881ed9-9110-4370-85ab-4c40e879b3ba")
+)
+--create minimal long dataset of vaccine transactions to transpose 
+
+SELECT DISTINCT person_id, 
+    data_partner_id,
+    drug_exposure_start_date as vax_date,
+    vax_type
+FROM baseline_vaccines_testing
+
+UNION 
+
+SELECT DISTINCT person_id,
+    data_partner_id,
+    procedure_date as vax_date,
+    vax_type
+FROM baseline_vaccines_from_proc_testing
+
