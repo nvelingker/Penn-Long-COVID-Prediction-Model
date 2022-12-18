0604ab2143f5d7823571c1a922c83a02355c176b
Foundry (vector-client-token) <>
2022-12-18 21:20:56 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -8037,6 +8037,7 @@ def train_sequential_model_rebalance(train_sequential_model_3, produce_dataset):
 def train_test_model(all_patients_summary_fact_table_de_id, all_patients_summary_fact_table_de_id_testing, Long_COVID_Silver_Standard):
     
     static_cols = ['person_id','total_visits', 'age']
+    print(len(static_cols-1))
 
     cols = static_cols + [col for col in all_patients_summary_fact_table_de_id.columns if 'indicator' in col]
     
@@ -8905,7 +8906,7 @@ def valid_mTan(train_sequential_model_3, produce_dataset):
 )
 def validation_metrics( train_test_model, train_test_top_k_model, valid_mTan):
     train_test_top_k_model = train_test_top_k_model.drop("outcome", axis=1)
-    df = train_test_model.merge(train_test_top_k_model, on="person_id", how="left").merge(valid_mTan, on="person_id", how="inner")
+    df = train_test_model.merge(train_test_top_k_model, on="person_id", how="left").merge(valid_mTan, on="person_id", how="left")
 
     outcomes = [i for i in df.columns if i.endswith("_outcome") and not "ens" in i and not "nn" in i]
     print(outcomes)
